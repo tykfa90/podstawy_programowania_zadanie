f2d379ecee9c3160abaedcbba3fb9e9da7dd30eb
Konstanty Białek <tykfa90@gmail.com>
2020-06-03 13:01:37 +0200

    Zadanie 1

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Numerics;
+using System.Runtime.CompilerServices;
 
 namespace podstawy_programowania_zadanie
 {
@@ -6,7 +8,18 @@ namespace podstawy_programowania_zadanie
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Zadanie 1: a)");
+
+            Console.WriteLine(Program.Silnia(12));
+            Console.ReadLine();
+        }
+
+        private static BigInteger Silnia(int liczba)
+        {
+            if (liczba == 0)
+                return 1;
+            else
+                return liczba * Silnia(liczba - 1);
         }
     }
 }
