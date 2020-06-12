2a0e5c94e92315fc28ba68d117cd25ba504ba1ab
Konstanty Białek <tykfa90@gmail.com>
2020-06-12 15:34:23 +0200

    Zadanie 4 w toku

--- Program.cs
+++ Program.cs
@@ -8,19 +8,44 @@ namespace podstawy_programowania_zadanie
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Zadanie 1: a)");
-            Console.WriteLine("Podaj podstawę silni: ");
-            int liczba = int.Parse(Console.ReadLine());
-            Console.WriteLine("Wynik dla silni o podstawie: " + liczba + " to: " + Program.Silnia(liczba));
-            Console.ReadLine();
+            Console.WriteLine("BANKOMAT \n");
+            Console.WriteLine("Wybierz operację:\n" +
+                "1. Wypłata gotówki.\n" +
+                "2. Sprawdzenie stanu konta.\n" +
+                "3. Wyjście.");
+            string action = Console.ReadLine();
+            switch (action)
+            {
+                case "1":
+                    Program.Wyplata();
+                    break;
+                case "2":
+                    break;
+                case "3":
+                    break;
+                default:
+                    break;
+            }
         }
 
-        private static BigInteger Silnia(int liczba)
+        private static void Wyplata()
         {
-            if (liczba == 0)
-                return 1;
-            else
-                return liczba * Silnia(liczba - 1);
+            Console.WriteLine("Proszę podać kwotę do wypłaty: ");
+            bool running = true;
+            while (running)
+            {
+                string userAmount = Console.ReadLine();
+                bool check = int.TryParse(userAmount, out int amount);
+                if (check == true || amount != 0 || amount % 10 == 0)
+                {
+                    
+                    running = false;
+                }
+                else
+                {
+                    Console.WriteLine("Proszę wprowadzić wielokrotność kwoty 10 zł. Proszę ponownie wybrać kwotę do wypłaty: ");
+                }
+            }
         }
     }
 }
